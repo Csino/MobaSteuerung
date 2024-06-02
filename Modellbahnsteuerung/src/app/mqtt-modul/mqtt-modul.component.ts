0618c90da030d6c90f8ae3f1207e74cbb1ed5bb4
Csino <csino.moba@outlook.de>
2024-06-02 22:43:56 +0200

    Mqtt Verbindung 1

--- Modellbahnsteuerung/src/app/mqtt-modul/mqtt-modul.component.ts
+++ Modellbahnsteuerung/src/app/mqtt-modul/mqtt-modul.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { SignalDashboardComponent } from "../signal-dashboard/signal-dashboard.component";
+import mqtt from "mqtt";
+import { ButtonSignalComponent } from "../signal-dashboard/Button-Signal/Button-Signal.component";
 
 @Component({
-  selector: 'app-mqtt-modul',
-  standalone: true,
-  imports: [],
-  templateUrl: './mqtt-modul.component.html',
-  styleUrl: './mqtt-modul.component.scss'
+    selector: 'app-mqtt-modul',
+    standalone: true,
+    templateUrl: './mqtt-modul.component.html',
+    styleUrl: './mqtt-modul.component.scss',
+    imports: [SignalDashboardComponent, ButtonSignalComponent]
 })
 export class MqttModulComponent {
-
+[x: string]: any;
 }
 
+  const client = mqtt.connect("mqtt://192.168.2.135:8083"); // create a client
+
+
+client.on("connect", () => {
+      console.log ("verbunden");
+    } 
+)
+
+
+const Signalmessage = () => {
+  client.publish("/Moba/ESPNetzwerk", "Hp0");
+}
